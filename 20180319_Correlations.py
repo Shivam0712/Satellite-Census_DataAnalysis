44a8af22f976fae68bf611c9aa370aba1a670b2b
Shivam0712 <shivam.pathak.cer11@itbhu.ac.in>
2018-03-19 15:02:25 +0530

    Update and rename Untitled3.py to 20180319_Correlations.py

--- 20180319_Correlations.py
+++ 20180319_Correlations.py
@@ -38,40 +38,37 @@ df_HH=pd.read_csv("/home/tempuser/Doc/Correlations/2011HH_India.csv", encoding =
 # In[5]:
 
 
-#Creating the ID Variable ="Village Code" +"Ward"
+###Creating the ID Variable ="Village Code" +"Ward"
 df_PA['NewID']=df_PA['Town/Village'].astype(str)+"-"+df_PA['Ward'].astype(str)
 df_HH['NewID']=df_HH['7'].astype(str)+"-"+df_HH['8'].astype(str)
 
+###Combinning the dataframes
 df_comb=df_PA.merge(df_HH, left_on='NewID', right_on='NewID', how='inner')
 df_comb=df_comb.drop_duplicates()
 
+###Deleting unused dataframes
 del df_PA
 del df_HH
 
 
 # In[6]:
 
-
+### Useful Columns
 col= ['State', 'District', 'Subdistt', 'Town/Village', 'Ward', 'EB', 'Level', 'Name', 'TRU', 'No_HH', 'TOT_P', 'TOT_M', 'TOT_F', 'P_06', 'M_06', 'F_06', 'P_LIT', 'M_LIT', 'F_LIT', 'TOT_WORK_P', 'TOT_WORK_M', 'TOT_WORK_F', 'MAINWORK_P', 'MAINWORK_M', 'MAINWORK_F', 'MAIN_CL_P', 'MAIN_CL_M', 'MAIN_CL_F', 'MAIN_AL_P', 'MAIN_AL_M', 'MAIN_AL_F', 'MAIN_HH_P', 'MAIN_HH_M', 'MAIN_HH_F', 'MAIN_OT_P', 'MAIN_OT_M', 'MAIN_OT_F', 'MARGWORK_P', 'MARGWORK_M', 'MARGWORK_F', 'MARG_CL_P', 'MARG_CL_M', 'MARG_CL_F', 'MARG_AL_P', 'MARG_AL_M', 'MARG_AL_F', 'MARG_HH_P', 'MARG_HH_M', 'MARG_HH_F', 'MARG_OT_P', 'MARG_OT_M', 'MARG_OT_F', 'MARGWORK_3_6_P', 'MARGWORK_3_6_M', 'MARGWORK_3_6_F', 'MARG_CL_3_6_P', 'MARG_CL_3_6_M', 'MARG_CL_3_6_F', 'MARG_AL_3_6_P', 'MARG_AL_3_6_M', 'MARG_AL_3_6_F', 'MARG_HH_3_6_P', 'MARG_HH_3_6_M', 'MARG_HH_3_6_F', 'MARG_OT_3_6_P', 'MARG_OT_3_6_M', 'MARG_OT_3_6_F', 'MARGWORK_0_3_P', 'MARGWORK_0_3_M', 'MARGWORK_0_3_F', 'MARG_CL_0_3_P', 'MARG_CL_0_3_M', 'MARG_CL_0_3_F', 'MARG_AL_0_3_P', 'MARG_AL_0_3_M', 'MARG_AL_0_3_F', 'MARG_HH_0_3_P', 'MARG_HH_0_3_M', 'MARG_HH_0_3_F', 'MARG_OT_0_3_P', 'MARG_OT_0_3_M', 'MARG_OT_0_3_F', 'NON_WORK_P', 'NON_WORK_M', 'NON_WORK_F', '12', '13', '14', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '127', '128', '129', '130', '131', '132', '133', '134', '135', '136', '137', '138', '139']
 
 
 # In[7]:
 
-
+### Subsetting data frame with only useful columns
 df_comb=df_comb.loc[:,col]
-
-
-# In[37]:
-
-
-X=np.array(df_comb.loc[:,['23', '24', '25', '26', '27', '28', '29', '30', '31']])
 
 
 # In[8]:
 
-
+### HH amenities table variable
 var=['12', '13', '14', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '127', '128', '129', '130', '131', '132', '133', '134', '135', '136', '137', '138', '139']
 
+### Variable* Number of Household in Village * 0.01
 for i in var:
     df_comb[i]=df_comb[i]*df_comb['No_HH']*0.01
     df_comb[i]=df_comb[i].astype(int)
@@ -79,36 +76,28 @@ for i in var:
 
 # In[9]:
 
-
+### Aggregating data on district
 df_Dist= df_comb.groupby(['District'])['No_HH', 'TOT_P', 'TOT_M', 'TOT_F', 'P_06', 'M_06', 'F_06', 'P_LIT', 'M_LIT', 'F_LIT', 'TOT_WORK_P', 'TOT_WORK_M', 'TOT_WORK_F', 'MAINWORK_P', 'MAINWORK_M', 'MAINWORK_F', 'MAIN_CL_P', 'MAIN_CL_M', 'MAIN_CL_F', 'MAIN_AL_P', 'MAIN_AL_M', 'MAIN_AL_F', 'MAIN_HH_P', 'MAIN_HH_M', 'MAIN_HH_F', 'MAIN_OT_P', 'MAIN_OT_M', 'MAIN_OT_F', 'MARGWORK_P', 'MARGWORK_M', 'MARGWORK_F', 'MARG_CL_P', 'MARG_CL_M', 'MARG_CL_F', 'MARG_AL_P', 'MARG_AL_M', 'MARG_AL_F', 'MARG_HH_P', 'MARG_HH_M', 'MARG_HH_F', 'MARG_OT_P', 'MARG_OT_M', 'MARG_OT_F', 'MARGWORK_3_6_P', 'MARGWORK_3_6_M', 'MARGWORK_3_6_F', 'MARG_CL_3_6_P', 'MARG_CL_3_6_M', 'MARG_CL_3_6_F', 'MARG_AL_3_6_P', 'MARG_AL_3_6_M', 'MARG_AL_3_6_F', 'MARG_HH_3_6_P', 'MARG_HH_3_6_M', 'MARG_HH_3_6_F', 'MARG_OT_3_6_P', 'MARG_OT_3_6_M', 'MARG_OT_3_6_F', 'MARGWORK_0_3_P', 'MARGWORK_0_3_M', 'MARGWORK_0_3_F', 'MARG_CL_0_3_P', 'MARG_CL_0_3_M', 'MARG_CL_0_3_F', 'MARG_AL_0_3_P', 'MARG_AL_0_3_M', 'MARG_AL_0_3_F', 'MARG_HH_0_3_P', 'MARG_HH_0_3_M', 'MARG_HH_0_3_F', 'MARG_OT_0_3_P', 'MARG_OT_0_3_M', 'MARG_OT_0_3_F', 'NON_WORK_P', 'NON_WORK_M', 'NON_WORK_F', '12', '13', '14', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '127', '128', '129', '130', '131', '132', '133', '134', '135', '136', '137', '138', '139'].sum()
-
-
-# In[ ]:
-
-
-df_Dist[0:10]
-
 
 # In[10]:
 
 
+### District value for HH amenties in %
 for i in var:
     df_Dist[i]=df_Dist[i]/df_Dist['No_HH']
+
+### Calaculating primary abstract variables
 df_Dist['EMP_AL']=(df_Dist['MAIN_CL_P']+df_Dist['MAIN_AL_P'])/(df_Dist['TOT_P']-df_Dist['P_06'])
 df_Dist['EMP_NAL']=(df_Dist['MAIN_HH_P']+df_Dist['MAIN_OT_P'])/(df_Dist['TOT_P']-df_Dist['P_06'])
 df_Dist['LIT_RAT']=df_Dist['P_LIT']/df_Dist['TOT_P']
+
+### updating list of variables
 var= var + ['EMP_AL','EMP_NAL','LIT_RAT']
-
-
-# In[ ]:
-
-
-df_Dist.describe()
 
 
 # In[11]:
 
-
+### creating a dataframe with correlations
 df1=pd.DataFrame()
 df2=pd.DataFrame()
 
@@ -126,7 +115,7 @@ for i in range(len(var)):
 
 # In[12]:
 
-
+### Exporting the file with correlations
 df1.to_csv("correlations.csv") 
 
 
